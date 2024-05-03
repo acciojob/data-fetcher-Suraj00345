94e78fa088b9c508a9d21436480936ba1ea93004
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2024-05-03 22:26:54 +0530

    1st comit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if(loading) {
+    return<div> Loading...</div>
+  }
+
+  if(error){
+    return <div>An error occured : {error.message}</div>
+  }
+
+  if(!data){
+    return <div> No data found</div>
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data Fetch From API</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
